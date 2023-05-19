51748c84175e0a0eb9a0a5cae61388451d722301
cristi <cristisatcovschi123@gmail.com>
2023-05-19 22:31:57 +0300

    testing piano keyboard on mobile app

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,10 +1,100 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Audio } from "expo-av";
+
+const keys = [
+  {
+    color: "white",
+    name: "c",
+    sound: require("../sounds/C3.mp3"),
+  },
+  { color: "black", name: "" },
+  {
+    color: "white",
+    name: "d",
+    sound: require("../sounds/D3.mp3"),
+  },
+  { color: "black", name: "" },
+  {
+    color: "white",
+    name: "e",
+    sound: require("../sounds/E3.mp3"),
+  },
+  {
+    color: "white",
+    name: "f",
+    sound: require("../sounds/F3.mp3"),
+  },
+  { color: "black", name: "" },
+  {
+    color: "white",
+    name: "g",
+    sound: require("../sounds/G3.mp3"),
+  },
+  { color: "black", name: "" },
+  {
+    color: "white",
+    name: "a",
+    sound: require("../sounds/A4.mp3"),
+  },
+  { color: "black", name: "" },
+  {
+    color: "white",
+    name: "b",
+    sound: require("../sounds/B4.mp3"),
+  },
+  {
+    color: "white",
+    name: "c",
+    sound: require("../sounds/C4.mp3"),
+  },
+];
+
+const Piano = () => {
+  const playSound = (name, sound) => {
+    if (!sound) return;
+
+    console.log("Playing " + name);
+    Audio.Sound.createAsync(sound, { shouldPlay: true })
+      .then((res) => {
+        res.sound.setOnPlaybackStatusUpdate((status) => {
+          if (!status.didJustFinish) return;
+
+          // TODO: create global notification dispathcer to handle errores and show them in the UI
+          res.sound.unloadAsync().catch((error) => {
+            console.error(error);
+          });
+        });
+      })
+      .catch((error) => {});
+  };
+
+  return (
+    <View>
+      <View className="flex h-full flex-row justify-evenly">
+        {keys.map((key, idx) => (
+          <TouchableOpacity
+            onPressIn={() => playSound(key.name, key.sound)}
+            className="flex h-full flex-grow"
+            key={idx}
+          >
+            <View
+              className="h-full items-center justify-end border pb-4"
+              style={{ backgroundColor: key.color }}
+            >
+              <Text className="text-2xl">{key.name}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
 
 const Index = () => {
   return (
     <View>
-      <Text>editor</Text>
+      <Piano />
     </View>
   );
 };
